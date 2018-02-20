b93659a865f88e715792ea9237549a12d02d4edb
omar.bautista <omar.bautista@unad.edu.co>
2018-02-20 13:05:02 -0500

    added factors and libraries

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 	<title></title>
 	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<link rel="stylesheet" href="css/bootstrap.min.css">
   	<link rel="stylesheet" type="text/css" href="css/style.css">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<script src="js/jquery-3.3.1.min.js"></script>
+  	<script src="js/bootstrap.min.js"></script>
   	<script type="text/javascript" src="js/xlsx.full.min.js"></script>
   	<script type="text/javascript" src="js/FileSaver.min.js"></script>
   	<script type="text/javascript" src="js/exportData.js"></script>  	
--- js/fillTable.js
+++ js/fillTable.js
@@ -12,55 +12,74 @@ function fillTable() {
 	var sheet = wb.Sheets[wb.SheetNames[0]];
 	var rows = XLSX.utils.decode_range(sheet["!ref"]).e.r;
 	var cols = XLSX.utils.decode_range(sheet["!ref"]).e.c;
-	// document.getElementById("msg").innerHTML = rows;
 	var strRows = '';
 	for (var i = 3; i <= rows; i++) {
-		//console.log(wb.Sheets.SocioDemograficos['A'+(i+1)].v + " " + wb.Sheets.SocioDemograficos['D'+(i+1)].v);
 		strRows += "<tr>";
-		strRows += "<td>"+manageUndefined('A'+(i+1))+"</td>"; // IDENTIFICACION DEL ESTUDIANTE
+        strRows += "<td>"+(i-2)+"</td>"; // Nro.
+        strRows += "<td>"+manageUndefined('A'+(i+1))+"</td>"; // IDENTIFICACION DEL ESTUDIANTE
 		strRows += "<td>"+manageUndefined('B'+(i+1))+" "+manageUndefined('C'+(i+1))+" "+manageUndefined('D'+(i+1))+"</td>"; // NOMBRE COMPLETO
 		strRows += "<td>"+manageUndefined('F'+(i+1))+"</td>"; // CENTRO
 		strRows += "<td>"+manageUndefined('G'+(i+1))+"</td>"; // ZONA
 		strRows += "<td>"+manageUndefined('E'+(i+1))+"</td>"; // PROGRAMA
 		strRows += "<td>"+manageUndefined('H'+(i+1))+"</td>"; // ESCUELA
-		strRows += "<td>"+manageUndefined('I'+(i+1))+"</td>"; // EMAIL 
-		strRows += "<td>"+manageUndefined('CA'+(i+1))+"</td>"; // TELEFONO
+		strRows += "<td>"+manageUndefined('I'+(i+1))+"</td>"; // EMAIL
+		strRows += "<td></td>"; // TELEFONO
 		strRows += "<td>"+manageUndefined('AD'+(i+1))+"</td>"; // EDAD
 		strRows += "<td></td>"; // ASIGNACION
-		strRows += "<td>"+manageUndefined('X'+(i+1))+"</td>"; // CONVENIO
+		strRows += "<td></td>"; // CONVENIO
 		strRows += "<td></td>"; // NOVEDAD
-		strRows += "<td></td>"; // GENERAL
-		strRows += "<td></td>"; // CAMPUS VIRTUAL
-		var rFSD = riesgoFSD(i+1);
-		strRows += "<td"+addComment(rFSD, 'O'+(i-1))+">"+rFSD[0]+"</td>"; // Riesgo Factor Socio-Demográfico		
-		strRows += "<td></td>"; // Acciones realizadas según ruta de PAPC
-		strRows += "<td></td>"; // RESULTADOS
-		var rFSE = riesgoFSE(i+1);
-		strRows += "<td"+addComment(rFSE, 'R'+(i-1))+">"+rFSE[0]+"</td>"; // Riesgo Factor Socio-Económico		
-		strRows += "<td></td>"; // Acciones realizadas según ruta de PAPC
-		strRows += "<td></td>"; // RESULTADOS
-		strRows += "<td></td>"; // Riesgo Factor Academico Antecedentes		
-		strRows += "<td></td>"; // Acciones realizadas según ruta de PAPC
-		strRows += "<td></td>"; // RESULTADOS
-		strRows += "<td></td>"; // Riesgo Factor Académico por apropiacion al modelo		
-		strRows += "<td></td>"; // Acciones realizadas según ruta de PAPC
-		strRows += "<td></td>"; // RESULTADOS
-		strRows += "<td></td>"; // Riesgo Factor Institucional		
-		strRows += "<td></td>"; // Acciones realizadas según ruta de PAPC
-		strRows += "<td></td>"; // RESULTADOS
-		strRows += "<td></td>"; // Riesgo Factores Externos		
-		strRows += "<td></td>"; // Acciones realizadas según ruta de PAPC
-		strRows += "<td></td>"; // RESULTADOS
-		strRows += "<td></td>"; // NIVEL DE RIESGO POR FACTORES
-		strRows += "<td></td>"; // GRUPO COLABORATIVO
-		strRows += "<td></td>"; // SITUACION DE RIESGO
-		strRows += "<td></td>"; // ACCIONES REALIZADAS
-		strRows += "<td></td>"; // RESULTADOS
-		strRows += "<td></td>"; // No. Cursos 
-		strRows += "<td></td>"; // SITUACION DE RIESGO
-		strRows += "<td></td>"; // ACCIONES REALIZADAS
-		strRows += "<td></td>"; // RESULTADOS
-
+		strRows += "<td></td>"; // PARTICIPACION INDUCCION
+        strRows += "<td></td>"; // PARTICIPACION INMERSION CAMPUS VIRTUAL
+        var rFSD = validaRiesgos('FSD', i+1);
+		strRows += "<td"+addComment(rFSD, 'P'+(i+3))+">"+rFSD[0]+"</td>"; // Factor Socio-Demográfico
+		var rFPS = validaRiesgos('FPS', i+1);
+        strRows += "<td"+addComment(rFPS, 'Q'+(i+3))+">"+rFPS[0]+"</td>"; // Factor Psicosocial
+        var rFAA = validaRiesgos('FAA', i+1);
+        strRows += "<td"+addComment(rFAA, 'R'+(i+3))+">"+rFAA[0]+"</td>"; // Factor Académico Antecedentes
+        strRows += "<td></td>"; // Factor Socio-Económico
+        strRows += "<td></td>"; // Factor Académico por apropiacion al modelo
+        strRows += "<td></td>"; // Factor Institucional
+        strRows += "<td></td>"; // Factores Externos
+        strRows += "<td></td>"; // COMPETENCIAS DIGITALES BASICAS
+        strRows += "<td></td>"; // COMPETENCIAS CUANTITATIVAS
+        strRows += "<td></td>"; // COMPETENCIAS LECTO-ESCRITORA
+        strRows += "<td></td>"; // COMPETENCIAS DE INGLES
+        strRows += "<td></td>"; // MEDIO DE CONTACTO
+        strRows += "<td></td>"; // ACCIONES REALIZADAS SEGÚN RUTA DE PAPC
+        strRows += "<td></td>"; // RESULTADOS ACCIONES DE ACOMPAÑAMIENTO
+        strRows += "<td></td>"; // NIVEL DE RIESGO
+        strRows += "<td></td>"; // OBSERVACIONES
+        strRows += "<td></td>"; // GRUPO COLABORATIVO
+        strRows += "<td></td>"; // SITUACION DE RIESGO
+        strRows += "<td></td>"; // ACCIONES REALIZADAS
+        strRows += "<td></td>"; // RESULTADOS
+        strRows += "<td></td>"; // No. Cursos
+        strRows += "<td></td>"; // SITUACION DE RIESGO
+        strRows += "<td></td>"; // MEDIO DE CONTACTO
+        strRows += "<td></td>"; // ACCIONES REALIZADAS
+        strRows += "<td></td>"; // RESULTADOS
+        strRows += "<td></td>"; // SITUACION DE RIESGO
+        strRows += "<td></td>"; // ACCIONES REALIZADAS
+        strRows += "<td></td>"; // RESULTADOS
+        strRows += "<td></td>"; // SITUACION DE ALERTA
+        strRows += "<td></td>"; // COMPETENCIAS DIGITALES BASICAS
+        strRows += "<td></td>"; // COMPETENCIAS CUANTITATIVAS
+        strRows += "<td></td>"; // COMPETENCIAS LECTO-ESCRITORA
+        strRows += "<td></td>"; // COMPETENCIAS DE INGLES
+        strRows += "<td></td>"; // SITUACION DE RIESGO
+        strRows += "<td></td>"; // MEDIO DE CONTACTO
+        strRows += "<td></td>"; // ACCIONES REALIZADAS
+        strRows += "<td></td>"; // RESULTADOS
+        strRows += "<td></td>"; // SITUACION DE RIESGO
+        strRows += "<td></td>"; // ACCIONES REALIZADAS
+        strRows += "<td></td>"; // RESULTADOS
+        strRows += "<td></td>"; // SITUACION DE ALERTA
+        strRows += "<td></td>"; // MEDIO DE CONTACTO
+        strRows += "<td></td>"; // APROBACION DE LA CATEDRA
+        strRows += "<td></td>"; // APROBACION DE CURSOS
+        strRows += "<td></td>"; // CULMINACION DEL PERIODO ACADEMICO
+        strRows += "<td></td>"; // RESULTADO FINAL
+        strRows += "<td></td>"; // CONSEJERO ACADEMICO
 	}
 	document.getElementById("tableBody").innerHTML = strRows;
 }
@@ -76,25 +95,65 @@ function addComment(comment, cellExport) {
 }
 
 // Analisis de los riesgos
-function riesgoFSD(row) {
-	var sheet = wb.Sheets[wb.SheetNames[0]];
-    var na = ['No Aplica', '#N/A'];
-    var riesgos = new Array;
-    riesgos.push(sheet['N'+row].v == 'Rural'?'Area Residencia:\n\tRural':null);
-    riesgos.push(sheet['J'+row].v== 'Madre Soltera'?'Estado Civil:\n\tMadre Soltera':null);
-    riesgos.push(na.indexOf(sheet['R'+row].v) < 0?'Disc. Auditiva:\n\t'+sheet['R'+row].v:null);
-    riesgos.push(na.indexOf(sheet['S'+row].v) < 0?'Disc. Cognitiva:\n\t'+sheet['S'+row].v:null);
-    riesgos.push(na.indexOf(sheet['U'+row].v) < 0?'Disc. Emocional:\n\t'+sheet['U'+row].v:null);
-    riesgos.push(na.indexOf(sheet['T'+row].v) < 0?'Disc. Fisica:\n\t'+sheet['T'+row].v:null);
-    riesgos.push(na.indexOf(sheet['V'+row].v) < 0?'Disc. Mental:\n\t'+sheet['V'+row].v:null);
-    riesgos.push(na.indexOf(sheet['Q'+row].v) < 0?'Disc. Visual:\n\t'+sheet['Q'+row].v:null);
-    riesgos.push(sheet['X'+row].v == 'Interno'?'Convenio INPEC:\n\tInterno':null);
-    msgFSD = '';
-    riesgos.forEach(function(e){
-	    msgFSD += (e!=null?'* '+e+'\n':'');
-	});
-	return msgFSD.length > 0?["Riesgo por condiciones personales",msgFSD]:["Sin riesgo por condiciones personales"];
+// Factor Socio-Demográfico
+function riesgoFSD(row, sheet, cells, na) {
+    cells.push(na.indexOf(sheet['L'+row].v.toString().toLowerCase()) < 0?'Etnia:\n\t'+sheet['L'+row].v:null);
+    cells.push(sheet['N'+row].v.toString().toLowerCase() == 'rural'?'Area Residencia:\n\tRural':null);
+    cells.push(sheet['P'+row].v.toString().toLowerCase() == 'si'?'Desplazado:\n\tSi':null);
+    cells.push(na.indexOf(sheet['R'+row].v.toString().toLowerCase()) < 0?'Disc. Auditiva:\n\t'+sheet['R'+row].v:null);
+    cells.push(na.indexOf(sheet['S'+row].v.toString().toLowerCase()) < 0?'Disc. Cognitiva:\n\t'+sheet['S'+row].v:null);
+    cells.push(na.indexOf(sheet['U'+row].v.toString().toLowerCase()) < 0?'Disc. Emocional:\n\t'+sheet['U'+row].v:null);
+    cells.push(na.indexOf(sheet['T'+row].v.toString().toLowerCase()) < 0?'Disc. Fisica:\n\t'+sheet['T'+row].v:null);
+    cells.push(na.indexOf(sheet['Q'+row].v.toString().toLowerCase()) < 0?'Disc. Visual:\n\t'+sheet['Q'+row].v:null);
+    cells.push(sheet['X'+row].v.toString().toLowerCase() == 'interno'?'Convenio INPEC:\n\tInterno':null);
+    cells.push(sheet['AD'+row].v >= 43?'Edad:\n\t'+sheet['AD'+row].v+' años':null);
 }
+
+// Factor Psicosocial
+function riesgoFPS(row, sheet, cells, na) {
+    cells.push(na.indexOf(sheet['V'+row].v.toString().toLowerCase()) < 0?'Disc. Mental:\n\t'+sheet['V'+row].v:null);
+    cells.push(na.indexOf(sheet['W'+row].v.toString().toLowerCase()) < 0?'Enfermedad:\n\t'+sheet['W'+row].v:null);
+}
+
+// Factor Académico Antecedentes
+function riesgoFAA(row, sheet, cells, na) {
+    cells.push(sheet['BI'+row].v.toString().toLowerCase() == 'no'?'Tomado Cursos Virtuales:\n\tNo':null);
+    cells.push(sheet['BK'+row].v.toString().toLowerCase() == '5 años o mas'?'Tiempo sin Estudiar:\n\t5 años o mas':null);
+    cells.push(sheet['BN'+row].v.toString().toLowerCase() == 'no'?'Primer opción de estudio:\n\tNo':null);
+}
+
+function prepareComment(cells) {
+    var msg = '';
+    cells.forEach(function(e){
+        msg += (e!=null?'* '+e+'\n':'');
+    });
+    return msg;
+}
+
+function validaRiesgos(factor, row) {
+	var sheet = wb.Sheets[wb.SheetNames[0]];
+    var na = ['no aplica', '#n/a', 'no pertenece', 'ninguna'];
+    var cells = new Array;
+    switch (factor) {
+        case 'FSD':
+            riesgoFSD(row, sheet, cells, na);
+            var msg = prepareComment(cells);
+            return msg.length > 0?["Riesgo por condiciones personales",msg]:["Sin riesgo por condiciones personales"];
+            break;
+        case 'FPS':
+            riesgoFPS(row, sheet, cells, na);
+            var msg = prepareComment(cells);
+            return msg.length > 0?["Riesgo por situaciones personales",msg]:["Sin riesgo por situaciones personales"];
+            break;
+        case 'FAA':
+            riesgoFAA(row, sheet, cells, na);
+            var msg = prepareComment(cells);
+            return msg.length > 0?["Riesgo por antecedentes académicos",msg]:["Sin Riesgo por antecedentes académicos"];
+            break;
+    }
+}
+
+
 
 function riesgoFSE(row) {
 	var sheet = wb.Sheets[wb.SheetNames[0]];
